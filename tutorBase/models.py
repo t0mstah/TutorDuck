70a6567d54296e287ca142934cbb78a0b90695fc
Tommy Fang <tfang17@stanford.edu>
2014-12-22 15:49:39 -0700

    spacing

--- tutorBase/models.py
+++ tutorBase/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-
 class User(models.Model):
     email = models.EmailField()
     password = models.CharField(max_length=200)
-
 
 class TutorCard(models.Model):
     tutor = models.ForeignKey(User)
@@ -15,6 +13,3 @@ class TutorCard(models.Model):
     description = models.CharField(max_length=200)
     rate = models.CommaSeparatedIntegerField(max_length=10)
     phone = models.IntegerField()
-
-
-
