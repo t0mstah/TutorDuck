96b564b81d3242b203abdb111cc9babd5790f8cb
Tommy Fang <tfang17@stanford.edu>
2014-12-22 16:08:57 -0700

    pull

--- tutorBase/urls.py
+++ tutorBase/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^post/login.html$',
+    url(r'^login.html$',
         'tutorBase.views.login', name='login'),
 )
--- tutorBase/views.py
+++ tutorBase/views.py
@@ -10,6 +10,8 @@ def login(request):
     if request.method == 'POST':
         form = loginForm(request.POST)
         if form.is_valid():
+            email = form.cleaned_data['Email']
+            password = form.cleaned_data['Password']
             return HttpResponseRedirect('/who/')
     else:
         form = loginForm()
